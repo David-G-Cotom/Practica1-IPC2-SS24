4f8ecdc4905b3fca80e1f72a7dbc36b70af23581
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2024-08-08 23:17:10 -0600

    Configurando JDBC

--- Practica1_SS24/src/main/java/backend/data/ConexionDB.java
+++ Practica1_SS24/src/main/java/backend/data/ConexionDB.java
@@ -4,10 +4,35 @@
  */
 package backend.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Carlos Cotom
  */
 public class ConexionDB {
+    
+    private static final String URL_MYSQL = "jdbc:mysql://localhost:3306/bd_banco";
+    private static final String USER = "root";
+    private static final String PASSWORD = "uipc2w8e9u1ner";
+    private static Connection connection;
+
+    public ConexionDB() {
+        try {
+            connection = DriverManager.getConnection(USER, USER, PASSWORD);
+        } catch (SQLException ex) {
+            System.out.println("Error al conectarse con MySQL");
+        }
+    }
+    
+    public static Connection getConnection() {
+        if (connection == null) {
+            new ConexionDB();
+            System.out.println("Conexion Exitosa");
+        }
+        return connection;
+    }
     
 }
